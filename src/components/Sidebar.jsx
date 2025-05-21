257815eebd9fb97b6bbcff349655350f7961277d
Alex David Frías <adavidfruiz@gmail.com>
2025-05-21 16:56:47 -0500

    fix: adjust Sidebar component to ensure full height display

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,6 +1,6 @@
 export default function Sidebar() {
   return (
-    <div className="w-64 bg-teal-800 text-white flex flex-col py-6 px-4 rounded-xl">
+    <div className="h-screen w-64 bg-teal-800 text-white flex flex-col py-6 px-4 rounded-xl">
       <h2 className="text-2xl font-bold mb-10 text-center">TASK MANAGEMENT</h2>
       <nav className="flex flex-col gap-6">
         <div className="bg-white/20 px-4 py-2 rounded-lg text-orange-300 font-semibold">
