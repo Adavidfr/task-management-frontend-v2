eabb2d2d322353516c8d396ae61a607ceafb9022
Alex David Frías <adavidfruiz@gmail.com>
2025-05-21 00:22:48 -0500

    refactor: restructure App component layout for improved readability

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,29 @@ import React from "react";
 import Sidebar from "./components/Sidebar";
 import Navbar from "./components/Navbar";
 import TaskList from "./components/TaskList";
+// import CalendarView from "./components/Calendar/CalendarView";
 
 function App() {
+  // return (
+  //   <div className="min-h-screen flex bg-background p-6">
+  //     <Sidebar />
+  //     <main className="min-h-screen bg-gray-100 px-6 flex-grow">
+  //       <Navbar />
+  //       <div className="flex justify-between gap-6">
+  //         <TaskList />
+  //         <CalendarView />
+  //       </div>
+  //     </main>
+  //   </div>
+  // );
+
   return (
     <div className="min-h-screen flex bg-background p-6">
       <Sidebar />
-      <Navbar />
-      <TaskList />
+      <main className="min-h-screen bg-gray-100 px-6 flex-grow">
+        <Navbar />
+        <TaskList />
+      </main>
     </div>
   );
 }
