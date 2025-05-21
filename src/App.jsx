31d5cf6f7805d5570c6533cf49767e4d8c99c92f
Alex David Frías <adavidfruiz@gmail.com>
2025-05-21 16:56:35 -0500

    refactor: update App component layout to include CalendarView and MembersList

--- src/App.jsx
+++ src/App.jsx
@@ -3,28 +3,29 @@ import React from "react";
 import Sidebar from "./components/Sidebar";
 import Navbar from "./components/Navbar";
 import TaskList from "./components/TaskList";
-// import CalendarView from "./components/Calendar/CalendarView";
+import CalendarView from "./components/CalendarView";
+import MembersList from "./components/MembersList";
 
 function App() {
-  // return (
-  //   <div className="min-h-screen flex bg-background p-6">
-  //     <Sidebar />
-  //     <main className="min-h-screen bg-gray-100 px-6 flex-grow">
-  //       <Navbar />
-  //       <div className="flex justify-between gap-6">
-  //         <TaskList />
-  //         <CalendarView />
-  //       </div>
-  //     </main>
-  //   </div>
-  // );
-
   return (
     <div className="min-h-screen flex bg-background p-6">
       <Sidebar />
       <main className="min-h-screen bg-gray-100 px-6 flex-grow">
         <Navbar />
-        <TaskList />
+        <div className="flex justify-between gap-6 h-[calc(100vh-100px)]">
+          <div className="flex-[3] h-full overflow-y-auto">
+            <TaskList />
+          </div>
+
+          <div className="flex-[2] flex flex-col gap-4">
+            <div className="flex-1">
+              <CalendarView />
+            </div>
+            <div className="flex-1">
+              <MembersList />
+            </div>
+          </div>
+        </div>
       </main>
     </div>
   );
